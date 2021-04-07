5aca63066a7caeb729d590200b807ae21abdd25c
MarceloCLopes <marc01mcl@gmail.com>
2021-04-07 13:11:03 -0300

    updated main activity class

--- app/src/main/java/com/mcl/roomdemo/MainActivity.kt
+++ app/src/main/java/com/mcl/roomdemo/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.mcl.roomdemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.mcl.roomdemo.databinding.ActivityMainBinding
+import com.mcl.roomdemo.db.SubscriberDatabase
+import com.mcl.roomdemo.db.SubscriberRepository
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var subscriberViewModel: SubscriberViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val dao = SubscriberDatabase.getInstance(application).subscriberDAO
+        val repository = SubscriberRepository(dao)
+        val factory = SubscriberViewModelFactory(repository)
+
     }
 }
